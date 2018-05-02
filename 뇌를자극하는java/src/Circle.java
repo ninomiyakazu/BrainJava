7e0ac3d1f8a20d9c1947c4d924815df956f78f4e
Administrator <Administrator@JBH>
2018-05-02 10:36:06 +0900

    0502 업로드

---
+++ 뇌를자극하는java/src/Circle.java
@@ -0,0 +1,42 @@
+/**
+ * 2018. 5. 2. Dev By JBH
+   
+   Circle.java
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+public class Circle {
+     public int radius;
+     public String name;
+     
+     public Circle() {}
+
+     public double getArea() { 	
+ 		return 3.14*radius*radius;
+ 	}
+
+ 	public static void main(String[] args) {
+ 		Circle pizza; 
+ 		pizza = new Circle(); 					
+ 		pizza.radius = 10; 						
+ 		pizza.name = "자바피자"; 			
+ 		double area = pizza.getArea(); 		
+ 		System.out.println(pizza.name + "의 면적은  " + area);
+
+ 		Circle donut = new Circle(); 		
+ 		donut.radius = 2; 						
+ 		donut.name = "자바도넛"; 			
+ 		area = donut.getArea(); 				
+ 		System.out.println(donut.name + "의 면적은" + area);
+ 	}
+ }
+
+     
+     
+     
+     
+
+
