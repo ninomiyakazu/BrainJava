b5630c6d0e340d547579cffe3fcd496f13fea5ed
Administrator <Administrator@JBH>
2018-05-02 11:08:11 +0900

    0502 업로드

--- 뇌를자극하는java/src/Circle.java
+++ 뇌를자극하는java/src/Circle.java
@@ -12,25 +12,38 @@ public class Circle {
      public int radius;
      public String name;
      
-     public Circle() {}
-
-     public double getArea() { 	
- 		return 3.14*radius*radius;
+     //매개변수 없는 생성자 
+     public Circle() {
+        radius = 1;
+        name = "";
+     }   
+     
+     //매개변수를 가진 생성자
+     public Circle(int r, String n) {
+           radius = r;
+           name = n;
  	}
-
+     
+     //메소드 
+     public double getArea() { 	
+  		return 3.14*radius*radius;
+     }
+     
+    //메인메소드
  	public static void main(String[] args) {
- 		Circle pizza; 
- 		pizza = new Circle(); 					
- 		pizza.radius = 10; 						
- 		pizza.name = "자바피자"; 			
- 		double area = pizza.getArea(); 		
- 		System.out.println(pizza.name + "의 면적은  " + area);
-
- 		Circle donut = new Circle(); 		
- 		donut.radius = 2; 						
- 		donut.name = "자바도넛"; 			
- 		area = donut.getArea(); 				
- 		System.out.println(donut.name + "의 면적은" + area);
+ 		
+ 		Circle pizza = new Circle(10, "자바피자");
+ 		double pizza_area = pizza.getArea();	
+ 	    System.out.println(pizza.name + "의 면적은" + pizza_area);
+ 	/*	
+ 		Circle donut  = new Circle();
+ 		donut.radius=2;
+ 		donut.name = "자바도넛";
+ 		
+ 		double dount_area = donut.getArea();
+ 		System.out.println(dount.name + "의 면적은  " + donut area);
+ 		System.out.println(pizza.name + "의 면적은  " + pizza area);
+ 		*/
  	}
  }
 
